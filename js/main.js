53484020af426b3f53bbd0f5c60bc209fc9a86bc
Kazuho-Shibuya <businessfive25@gmail.com>
2021-05-16 00:15:14 +0900

    入力フォームからリストに値を追加するイベントを追加

--- js/main.js
+++ js/main.js
@@ -4,7 +4,13 @@
   var vm = new Vue({
     el: "#app",
     data: {
+      newItem: "",
       todos: ["task1", "task2", "task3"],
+    },
+    methods: {
+      addItem: function () {
+        this.todos.push(this.newItem);
+      },
     },
   });
 })();
